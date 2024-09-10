063498606d98bc421c9293d792256bc7bcecabcd
Igor Alves <76858556+igorwho08@users.noreply.github.com>
2024-09-10 00:55:48 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Amor",
+        descricao : "1 Coríntios 13:4-7 O amor é paciente, o amor é bondoso. Não inveja, não se vangloria, não se orgulha. Não maltrata, não procura seus interesses, não se ira facilmente, não guarda rancor. O amor não se alegra com a injustiça, mas se alegra com a verdade. Suporta tudo, crê tudo, espera tudo, desculpa tudo.",
+        link : "https://www.bibliaon.com/versiculo/1_corintios_13_4-7/",
+        tags : "perdao amoroso descupa"
+    },
+    {
+        titulo: "Raiva",
+        descricao: "Efésios 4:26 Irai-vos, e não pequeis; não se ponha o sol sobre a vossa ira, nem deis lugar ao diabo.",
+        link: "https://www.bibliaon.com/versiculo/efesios_4_26/",
+        tags : "paciencia raivoso calma"
+    },
+    {
+        titulo: "Ansiedade",
+        descricao: "Filipenses 4:6-7 Não andeis ansiosos por coisa alguma; em tudo, porém, as vossas petições sejam conhecidas diante de Deus por orações e rogos, com ações de graças. E a paz de Deus, que excede todo o entendimento, guardará os vossos corações e os vossos pensamentos em Cristo Jesus.",
+        link: "https://www.bibliaon.com/versiculo/filipenses_4_6-7/",
+        tags : "ansioso desespero demora"
+    }
+];
